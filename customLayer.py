901361c5f31cddcb00fe032ef3d34f63ab7b3d4d
frank-fn10502 <frank08.chen@gmail.com>
2021-11-03 18:06:18 +0800

    add customLayer.py

---
+++ customLayer.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+
+# tensorflow.python.framework.ops.EagerTensor' object has no attribute '_keras_history'
+#為了解決以上的錯誤(主要是畫圖時會需要用到該屬性)
+class StochasticDropout(layers.Layer):
+    # from effNetV2 code #survival_prob 預設 0.8 def drop_connect(inputs, is_training = True, survival_prob = 0.8):
+    def call(self, inputs):
+        """Drop the entire conv with given survival probability."""
+        # "Deep Networks with Stochastic Depth", https://arxiv.org/pdf/1603.09382.pdf
+        if not is_training:
+            return inputs
+
+        # Compute tensor.
+        batch_size = tf.shape(inputs)[0]
+        random_tensor = survival_prob
+        random_tensor += tf.random.uniform([batch_size, 1, 1, 1], dtype=inputs.dtype)
+        binary_tensor = tf.floor(random_tensor)
+        # Unlike conventional way that multiply survival_prob at test time, here we
+        # divide survival_prob at training time, such that no addition compute is
+        # needed at test time.
+        output = inputs / survival_prob * binary_tensor
+        return output
--- frankModel.py
+++ frankModel.py
@@ -7,32 +7,7 @@ from tensorflow.python.keras.layers.merge import add
 
 from utils.outputs import ModelOuputHelper
 from utils.utils import VersionMark as verMark
-
-
-# from effNetV2 code #survival_prob 預設 0.8 by frank
-def drop_connect(inputs, is_training = True, survival_prob = 0.8):
-  """Drop the entire conv with given survival probability."""
-  # "Deep Networks with Stochastic Depth", https://arxiv.org/pdf/1603.09382.pdf
-  if not is_training:
-    return inputs
-
-  # Compute tensor.
-  batch_size = tf.shape(inputs)[0]
-  random_tensor = survival_prob
-  random_tensor += tf.random.uniform([batch_size, 1, 1, 1], dtype=inputs.dtype)
-  binary_tensor = tf.floor(random_tensor)
-  # Unlike conventional way that multiply survival_prob at test time, here we
-  # divide survival_prob at training time, such that no addition compute is
-  # needed at test time.
-  output = inputs / survival_prob * binary_tensor
-  return output
-
-
-# tensorflow.python.framework.ops.EagerTensor' object has no attribute '_keras_history'
-#為了解決以上的錯誤(主要是畫圖時會需要用到該屬性)
-class StochasticDropout(layers.Layer):
-    def call(self, inputs):
-        return drop_connect(inputs)
+from customLayer import StochasticDropout
 
 
 class BasicModel:
