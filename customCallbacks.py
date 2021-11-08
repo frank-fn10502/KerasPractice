e786bcb26930f26a82d91d922d457dd7e5640d77
frank-fn10502 <frank08.chen@gmail.com>
2021-11-08 17:02:23 +0800

    change callback print msg

--- customCallbacks.py
+++ customCallbacks.py
@@ -3,6 +3,6 @@ from tensorflow import keras
 
 class TestCallback(keras.callbacks.Callback):
     def on_epoch_begin(self, epoch, logs=None):
-        print(f"epoch: {epoch},\nmodel:{self.model},\ngot log keys:{logs.keys()}")
+        # print(f"epoch: {epoch},\nmodel:{self.model},\ngot log keys:{logs.keys()}")
         self.model.layers[1].setNewMagnitude(epoch)
-        print(f"epoch: {epoch},\nmodel:{self.model},\ngot log keys:{logs.keys()}")
+        print(f"magnitude: {self.model.layers[1].magnitude}")
